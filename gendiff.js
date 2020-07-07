f79f598dcc3aed192668488f96ff357a3a9a29aa
Кирилл <samsuu@ya.ru>
2020-07-07 19:05:32 +0700

    step 1 | add help file

---
+++ gendiff.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+import program from 'commander';
+
+program
+  .version ('1.0.0')
+  .description ('Compares two configuration files and shows a difference.')
+  .parse(process.argv);
+
+console.log(program.args);
