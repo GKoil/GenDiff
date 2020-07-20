32de276fd85a80f3bc6a32db8527e87fb08f02e7
Кирилл <samsuu@ya.ru>
2020-07-20 22:24:43 +0700

    step 5 | add .yaml format
    refactoring code
    add support yaml & yml format

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -7,16 +7,16 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 test('findDifferencesJSON', async () => {
-  const beforeFile = await fs.readFileSync(`${__dirname}/../__fixtures__/before.json`, 'utf8');
-  const afterFile = await fs.readFileSync(`${__dirname}/../__fixtures__/after.json`, 'utf8');
+  const beforeFile = `${__dirname}/../__fixtures__/before.json`;
+  const afterFile = `${__dirname}/../__fixtures__/after.json`;
   const result = await fs.readFileSync(`${__dirname}/../__fixtures__/result.txt`, 'utf8');
 
   const difference = gendiff(beforeFile, afterFile);
   expect(difference).toEqual(result);
 });
 test('findDifferencesYAML', async () => {
-  const beforeFileJSON = await fs.readFileSync(`${__dirname}/../__fixtures__/before.json`, 'utf8');
-  const afterFileYAML = await fs.readFileSync(`${__dirname}/../__fixtures__/after.yml`, 'utf8');
+  const beforeFileJSON = `${__dirname}/../__fixtures__/before.json`;
+  const afterFileYAML = `${__dirname}/../__fixtures__/after.yml`;
   const result = await fs.readFileSync(`${__dirname}/../__fixtures__/result.txt`, 'utf8');
 
   const difference = gendiff(beforeFileJSON, afterFileYAML);
--- src/index.js
+++ src/index.js
@@ -1,30 +1,29 @@
+import fs from 'fs';
 import path from 'path';
 import parser from './parsers.js';
 
-export default (fileBefore, fileAfter) => {
-  if (path.extname(fileBefore) === '.yml' || path.extname(fileAfter) === '.yml') {
-    return console.log('yml');
-  }
-
-  const ObjFileBefore = JSON.parse(fileBefore);
-  const ObjFileAfter = JSON.parse(fileAfter);
+export default (file1, file2) => {
+  const beforeFile = fs.readFileSync(path.resolve(process.cwd(), file1), 'utf8');
+  const afterFile = fs.readFileSync(path.resolve(process.cwd(), file2), 'utf8');
+  const parseBeforeFile = parser(beforeFile, file1);
+  const parseAfterFile = parser(afterFile, file2);
 
   let result = '';
 
-  const entriesFileBefore = Object.entries(ObjFileBefore);
-  const entriesFileAfter = Object.entries(ObjFileAfter);
+  const entriesFileBefore = Object.entries(parseBeforeFile);
+  const entriesFileAfter = Object.entries(parseAfterFile);
 
   entriesFileBefore.forEach(([key, value]) => {
-    if (ObjFileAfter[key] === value) {
+    if (parseAfterFile[key] === value) {
       result += `    ${key}: ${value} \n`;
-    } else if (ObjFileAfter[key] === undefined) {
+    } else if (parseAfterFile[key] === undefined) {
       result += `  - ${key}: ${value} \n`;
     } else {
-      result += `  + ${key}: ${ObjFileAfter[key]} \n  - ${key}: ${value} \n`;
+      result += `  + ${key}: ${parseAfterFile[key]} \n  - ${key}: ${value} \n`;
     }
   });
   entriesFileAfter.forEach(([key, value]) => {
-    if (ObjFileBefore[key] === undefined) {
+    if (parseBeforeFile[key] === undefined) {
       result += `  - ${key}: ${value} \n`;
     }
   });
