0ac4814fc242fcaca464df24047b18f9c08360f3
Кирилл <samsuu@ya.ru>
2020-10-02 12:38:31 +0700

    Make tests for nested structure
    Tests for nested files json/yaml/ini

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -7,8 +7,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 
-const findPathFile = (file) => `${__dirname}/../__fixtures__/${file}`;
-const readFile = (file) => fs.readFileSync(`${__dirname}/../__fixtures__/${file}`, 'utf8');
+const getFixturePath = (file) => `${__dirname}/../__fixtures__/${file}`;
+const readFile = (file) => fs.readFileSync(getFixturePath(file), 'utf8');
 
 let result;
 beforeEach(() => {
@@ -16,23 +16,23 @@ beforeEach(() => {
 })
 
 test('findDifferencesJSON', async () => {
-  const beforeFile = findPathFile('before.json');
-  const afterFile = findPathFile('after.json');
+  const beforeFile = getFixturePath('before.json');
+  const afterFile = getFixturePath('after.json');
   
 
   const difference = gendiff(beforeFile, afterFile);
   expect(difference).toEqual(result);
 });
 test('findDifferencesYAML', async () => {
-  const beforeFileJSON = findPathFile('before.json');
-  const afterFileYAML = findPathFile('after.yml');
+  const beforeFileJSON = getFixturePath('before.json');
+  const afterFileYAML = getFixturePath('after.yml');
 
   const difference = gendiff(beforeFileJSON, afterFileYAML);
   expect(difference).toEqual(result);
 });
 test('findDifferencesINI', async () => {
-  const beforeFileJSON = findPathFile('before.json');
-  const afterFileYAML = findPathFile('after.ini');
+  const beforeFileJSON = getFixturePath('before.json');
+  const afterFileYAML = getFixturePath('after.ini');
 
   const difference = gendiff(beforeFileJSON, afterFileYAML);
   expect(difference).toEqual(result);
