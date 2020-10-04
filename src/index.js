a89cdce583d2611229e6bf6b2e03bc6b1b5ff4e0
Кирилл <samsuu@ya.ru>
2020-10-04 17:25:40 +0700

    add AST

--- src/index.js
+++ src/index.js
@@ -3,33 +3,50 @@ import fs from 'fs';
 import path from 'path';
 import parser from './parsers.js';
 
+// const stylish = (object, key, sign = ' ') => `  ${sign} ${key}: ${object[key]}`;
+
+const getNode = (before, after) => {
+  const common = { ...before, ...after };
+  const keys = Object.keys(common);
+
+  return keys.reduce((acc, key) => {
+    const oldValue = _.has(before, key) ? before[key] : null;
+    const newValue = _.has(after, key) ? after[key] : null;
+    const getStatus = (value1, value2) => {
+      if (value1 === null) {
+        return 'add';
+      }
+      if (value2 === null) {
+        return 'delete';
+      }
+      if (value1 !== value2) {
+        return 'update';
+      }
+      return 'stay';
+    };
+    const status = getStatus(oldValue, newValue);
+    const children = _.isObject(common[key]) ? getNode(before[key], after[key]) : null;
+
+    const tree = {
+      key,
+      status,
+      values: {
+        oldValue,
+        newValue,
+      },
+      children,
+    };
+    acc.push(tree);
+    return acc;
+  }, []);
+};
+
 export default (file1, file2) => {
   const beforeFile = fs.readFileSync(path.resolve(process.cwd(), file1), 'utf8');
   const afterFile = fs.readFileSync(path.resolve(process.cwd(), file2), 'utf8');
   const parseBeforeFile = parser(beforeFile, file1);
   const parseAfterFile = parser(afterFile, file2);
 
-  const commonFile = { ...parseBeforeFile, ...parseAfterFile };
-  const keys = Object.keys(commonFile);
-
-  const getDiff = keys.reduce((acc, key) => {
-    if (!_.has(parseBeforeFile, key)) {
-      acc.push(`  + ${key}: ${parseAfterFile[key]}`);
-      return acc;
-    }
-    if (!_.has(parseAfterFile, key)) {
-      acc.push(`  - ${key}: ${parseBeforeFile[key]}`);
-      return acc;
-    }
-    if (parseBeforeFile[key] !== parseAfterFile[key]) {
-      acc.push(`  + ${key}: ${parseAfterFile[key]}`);
-      acc.push(`  - ${key}: ${parseBeforeFile[key]}`);
-      return acc;
-    }
-    acc.push(`    ${key}: ${commonFile[key]}`);
-    return acc;
-  }, [])
-    .join('\n');
-
-  return `{\n${getDiff}\n}`;
+  const diff = getNode(parseBeforeFile, parseAfterFile);
+  return JSON.stringify(diff, null, ' ');
 };
