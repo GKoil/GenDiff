dc41455309d50e094af0d1329f878bb2e0720459
Кирилл <samsuu@ya.ru>
2020-10-06 20:12:23 +0700

    add default stylish mode

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -8,10 +8,10 @@ program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format', 'stylish')
   .action((file1, file2) => {
-    const difference = diff(file1, file2);
+    const difference = diff(file1, file2, program.format);
     console.log(difference);
-  })
-  .option('-f, --format', 'output format');
+  });
 
 program.parse(process.argv);
