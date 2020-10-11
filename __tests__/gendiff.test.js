f9a484a0581e8ef94ba3b486a895c39a823e037b
Кирилл <samsuu@ya.ru>
2020-10-11 15:15:04 +0700

    Make tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -10,30 +10,29 @@ const __dirname = dirname(__filename);
 const getFixturePath = (file) => `${__dirname}/../__fixtures__/${file}`;
 const readFile = (file) => fs.readFileSync(getFixturePath(file), 'utf8');
 
-let result;
+let resultStylish;
 beforeEach(() => {
-  result = readFile('result.txt');
+  resultStylish = readFile('result-stylish.txt');
 })
 
 test('findDifferencesJSON', async () => {
   const beforeFile = getFixturePath('before.json');
   const afterFile = getFixturePath('after.json');
-  
 
-  const difference = gendiff(beforeFile, afterFile);
-  expect(difference).toEqual(result);
+  const differenceStylish = gendiff(beforeFile, afterFile);
+  expect(differenceStylish).toEqual(resultStylish);
 });
 test('findDifferencesYAML', async () => {
   const beforeFileJSON = getFixturePath('before.json');
   const afterFileYAML = getFixturePath('after.yml');
 
   const difference = gendiff(beforeFileJSON, afterFileYAML);
-  expect(difference).toEqual(result);
+  expect(difference).toEqual(resultStylish);
 });
 test('findDifferencesINI', async () => {
   const beforeFileJSON = getFixturePath('before.json');
   const afterFileYAML = getFixturePath('after.ini');
 
   const difference = gendiff(beforeFileJSON, afterFileYAML);
-  expect(difference).toEqual(result);
+  expect(difference).toEqual(resultStylish);
 });
