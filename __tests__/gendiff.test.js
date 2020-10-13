55f16aeee2f0704bd4e91839cd3fccb4a4fa5f93
Кирилл <samsuu@ya.ru>
2020-10-13 15:11:25 +0700

    Fix tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -11,28 +11,36 @@ const getFixturePath = (file) => `${__dirname}/../__fixtures__/${file}`;
 const readFile = (file) => fs.readFileSync(getFixturePath(file), 'utf8');
 
 let resultStylish;
+let resultPlain;
 beforeEach(() => {
   resultStylish = readFile('result-stylish.txt');
+  resultPlain = readFile('result-plain.txt');
 })
 
 test('findDifferencesJSON', async () => {
-  const beforeFile = getFixturePath('before.json');
-  const afterFile = getFixturePath('after.json');
+  const beforeFileJSON = getFixturePath('before.json');
+  const afterFileJSON = getFixturePath('after.json');
 
-  const differenceStylish = gendiff(beforeFile, afterFile);
+  const differenceStylish = gendiff(beforeFileJSON, afterFileJSON, 'stylish');
   expect(differenceStylish).toEqual(resultStylish);
+  const differencePlain = gendiff(beforeFileJSON, afterFileJSON, 'plain');
+  expect(differencePlain).toEqual(resultPlain);
 });
 test('findDifferencesYAML', async () => {
   const beforeFileJSON = getFixturePath('before.json');
   const afterFileYAML = getFixturePath('after.yml');
 
-  const difference = gendiff(beforeFileJSON, afterFileYAML);
-  expect(difference).toEqual(resultStylish);
+  const differenceStylish = gendiff(beforeFileJSON, afterFileYAML, 'stylish');
+  expect(differenceStylish).toEqual(resultStylish);
+  const differencePlain = gendiff(beforeFileJSON, afterFileYAML, 'plain');
+  expect(differencePlain).toEqual(resultPlain);
 });
 test('findDifferencesINI', async () => {
   const beforeFileJSON = getFixturePath('before.json');
-  const afterFileYAML = getFixturePath('after.ini');
+  const afterFileINI = getFixturePath('after.ini');
 
-  const difference = gendiff(beforeFileJSON, afterFileYAML);
-  expect(difference).toEqual(resultStylish);
+  const differenceStylish = gendiff(beforeFileJSON, afterFileINI, 'stylish');
+  expect(differenceStylish).toEqual(resultStylish);
+  const differencePlain = gendiff(beforeFileJSON, afterFileINI, 'plain');
+  expect(differencePlain).toEqual(resultPlain);
 });
