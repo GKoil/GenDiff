a90cabee993b4e7fd7ee07bbd2f54b60e894f2de
Кирилл <samsuu@ya.ru>
2020-10-30 20:41:54 +0700

    Refactorign formatter plain

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,50 +1,26 @@
 import _ from 'lodash';
-import {
-  getKey, getStatus, getValues, getOldValue, getNewValue, isList, getChildren,
-} from '../ast/node.js';
 
-const getLine = (key, status, values) => {
-  const oldValue = _.isObject(getOldValue(values)) ? '[complex value]' : getOldValue(values);
-  const newValue = _.isObject(getNewValue(values)) ? '[complex value]' : getNewValue(values);
-
-  switch (status) {
-    case 'update':
-      return `Property '${key.slice(1)}' was updated. From ${oldValue} to ${newValue}`;
-    case 'add':
-      return `Property '${key.slice(1)}' was added with value: ${newValue}`;
-    case 'delete':
-      return `Property '${key.slice(1)}' was removed`;
-    default:
-      return false;
-  }
-};
-
-const plain = (tree, stackKey = '') => {
-  const diff = tree.reduce((acc, node) => {
-    const key = getKey(node);
-    const status = getStatus(node);
-    const values = getValues(node);
-    const oldValue = getOldValue(values);
-    const newValue = getNewValue(values);
-    const newStackKey = `${stackKey}.${key}`;
-
-    if (isList(node)) {
-      const line = getLine(newStackKey, status, values);
-      return [...acc, line];
+const plain = (tree, stackKey = []) => {
+  const diff = tree.map((node) => {
+    const key = [...stackKey, node.key].join('.');
+    const { status } = node;
+    if (status === 'deleted') {
+      return `Property '${key}' was removed`;
     }
-
-    const isOldValueNotObject = _.isObject(oldValue) && !_.isObject(newValue);
-    const isNewValueNotObject = !_.isObject(oldValue) && _.isObject(newValue);
-    if (isOldValueNotObject || isNewValueNotObject) {
-      const line = getLine(newStackKey, status, values);
-      return [...acc, line];
+    if (status === 'added') {
+      const value = _.isObject(node.value) ? '[complex value]' : node.value;
+      return `Property '${key}' was ${status} with value: ${value}`;
     }
-
-    const children = getChildren(node);
-    const newNode = plain(children, newStackKey);
-    return [...acc, newNode];
-  }, [])
-    .flat();
+    if (status === 'updated') {
+      const oldValue = _.isObject(node.oldValue) ? '[complex value]' : node.oldValue;
+      const newValue = _.isObject(node.newValue) ? '[complex value]' : node.newValue;
+      return `Property '${key}' was ${status}. From ${oldValue} to ${newValue}`;
+    }
+    if (status === 'nested') {
+      return plain(node.children, [key]);
+    }
+    return null;
+  });
 
   return _.filter(diff).join('\n');
 };
