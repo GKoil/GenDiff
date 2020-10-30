35f60cc88f79ce9a00f2edb769baacd9e304d35d
Кирилл <samsuu@ya.ru>
2020-10-30 15:40:39 +0700

    Delete interface AST & relocate getAST

--- src/ast/node.js
+++
@@ -1,7 +0,0 @@
-export const getKey = (tree) => tree.key;
-export const getStatus = (tree) => tree.status;
-export const getValues = ({ values }) => values;
-export const getOldValue = (values) => values.oldValue;
-export const getNewValue = (values) => values.newValue;
-export const isList = (tree) => tree.children === null;
-export const getChildren = ({ children }) => children;
