aca7269114cdc9d1e8c4fc6e0eeb306551ddd24e
Кирилл <samsuu@ya.ru>
2020-10-30 15:41:29 +0700

    Refactoring formatter stylish and fix tests

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,62 +1,45 @@
 import _ from 'lodash';
-import {
-  getKey, getStatus, getValues, getOldValue, getNewValue, getChildren,
-} from '../ast/node.js';
 
-const getLine = (key, status, oldValue, newValue, spaces = '', list) => {
-  if (list === true) {
-    return `${spaces}  ${key}: ${newValue || oldValue}`;
-  }
-  if (status === 'add') {
-    return `${spaces}+ ${key}: ${newValue}`;
-  }
-  if (status === 'delete') {
-    return `${spaces}- ${key}: ${oldValue}`;
-  }
-  if (status === 'update') {
-    return `${spaces}- ${key}: ${oldValue}\n${spaces}+ ${key}: ${newValue}`;
-  }
-  return `${spaces}  ${key}: ${newValue}`;
+const getSign = {
+  added: '+',
+  deleted: '-',
+  'not changed': ' ',
 };
 
 const stylish = (value, spaceCount = 2) => {
-  const iter = (tree, depth, list) => {
+  const iter = (tree, depth) => {
     const replacer = ' ';
     const deepSpaceCount = spaceCount + depth;
     const spaces = replacer.repeat(deepSpaceCount);
     const currentSpaces = replacer.repeat(depth);
 
-    const lines = tree.reduce((acc, node) => {
-      const key = getKey(node);
-      const status = getStatus(node);
-      const values = getValues(node);
-      const oldValue = getOldValue(values);
-      const newValue = getNewValue(values);
-      const children = getChildren(node);
+    if (!_.isObject(tree)) {
+      return tree.toString();
+    }
 
-      if (!_.isObject(oldValue) && !_.isObject(newValue)) {
-        return [...acc, getLine(key, status, oldValue, newValue, spaces, list)];
+    if (!_.isArray(tree)) {
+      const values = Object
+        .entries(tree)
+        .map(([key, value]) => `${spaces}  ${key}: ${value}`);
+      return [
+        '{',
+        ...values,
+        `${currentSpaces}}`,
+      ].join('\n');
+    }
+    const lines = tree.map((node) => {
+      if (node.status === 'updated') {
+        const oldValue = iter(node.oldValue, deepSpaceCount);
+        const newValue = iter(node.newValue, deepSpaceCount);
+        return `${spaces}- ${node.key}: ${oldValue}\n${spaces}+ ${node.key}: ${newValue}`;
       }
-      if (_.isObject(oldValue) && _.isObject(newValue)) {
-        const makeNode = iter(children, deepSpaceCount);
-        return [...acc, `${spaces}  ${key}: ${makeNode}`];
+      if (node.status === 'nested') {
+        const makeNode = iter(node.children, deepSpaceCount);
+        return `${spaces}  ${node.key}: ${makeNode}`;
       }
-      const isOldValueNotObject = _.isObject(oldValue) && !_.isObject(newValue);
-      const isNewValueNotObject = !_.isObject(oldValue) && _.isObject(newValue);
-      if (isOldValueNotObject || isNewValueNotObject) {
-        const listValue = _.isObject(oldValue) ? newValue : oldValue;
-        const listStatus = !_.isObject(oldValue) ? 'delete' : 'add';
-        const nodeStatus = listStatus === 'delete' ? '+' : '-';
-
-        const makeList = getLine(key, listStatus, listValue, listValue, spaces);
-        const makeNode = iter(children, deepSpaceCount, true);
-
-        const makeString = `${makeList}\n${spaces}${nodeStatus} ${key}: ${makeNode}`;
-        return [...acc, makeString];
-      }
-
-      return acc;
-    }, []);
+      const value = iter(node.value);
+      return `${spaces}${getSign[node.status]} ${node.key}: ${value}`;
+    });
 
     return [
       '{',
@@ -67,4 +50,5 @@ const stylish = (value, spaceCount = 2) => {
 
   return iter(value, 0);
 };
+
 export default stylish;
