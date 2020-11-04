62d0500f6acc821c0762382bdd3c1c139fc5775e
Кирилл <samsuu@ya.ru>
2020-11-04 20:48:04 +0700

    Refactoring formatter 'stylish'. Make get spaces and get strings in functions

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,59 +1,49 @@
 import _ from 'lodash';
 
+const increaseDepth = (depth, spaceCount = 2) => depth + spaceCount;
+
+const getIndent = (count, replacer = ' ') => replacer.repeat(count);
 const getSpaces = (depth) => {
-  const replacer = ' ';
-  const spaceCount = 2;
-
-  const deepSpaceCount = spaceCount + depth;
-  const spaces = replacer.repeat(deepSpaceCount);
-  const currentSpaces = replacer.repeat(depth);
-
-  return [currentSpaces, spaces, deepSpaceCount];
+  const deepSpaceCount = increaseDepth(depth);
+  return getIndent(deepSpaceCount);
 };
+const getCurrentSpaces = (depth) => getIndent(depth);
+
+const getOutputTree = (lines, spaces) => `{\n${lines.join('\n')}\n${spaces}}`;
 
 const getValue = (value, depth) => {
   if (_.isObject(value)) {
-    const [currentSpaces, spaces] = getSpaces(depth);
     const lines = Object
       .entries(value)
-      .map(([key, objectValue]) => `${spaces}  ${key}: ${objectValue}`);
-    return [
-      '{',
-      ...lines,
-      `${currentSpaces}}`,
-    ].join('\n');
+      .map(([key, objectValue]) => `${getSpaces(depth)}  ${key}: ${objectValue}`);
+    return getOutputTree(lines, getCurrentSpaces(depth));
   }
   return value.toString();
 };
 
 const stylish = (data) => {
   const iter = (tree, depth) => {
-    const [currentSpaces, spaces, deepSpaceCount] = getSpaces(depth);
+    const spaces = getSpaces(depth);
     const lines = tree.map((node) => {
+      const increasedDepth = increaseDepth(depth);
       const { key, status } = node;
       switch (status) {
         case 'updated':
-          return `${spaces}- ${key}: ${getValue(node.oldValue, deepSpaceCount)}\n${spaces}+ ${key}: ${getValue(node.newValue, deepSpaceCount)}`;
+          return `${spaces}- ${key}: ${getValue(node.oldValue, increasedDepth)}\n${spaces}+ ${key}: ${getValue(node.newValue, increasedDepth)}`;
         case 'nested':
-          return `${spaces}  ${key}: ${iter(node.children, deepSpaceCount)}`;
+          return `${spaces}  ${key}: ${iter(node.children, increasedDepth)}`;
         case 'added':
-          return `${spaces}+ ${key}: ${getValue(node.value, deepSpaceCount)}`;
+          return `${spaces}+ ${key}: ${getValue(node.value, increasedDepth)}`;
         case 'deleted':
-          return `${spaces}- ${key}: ${getValue(node.value, deepSpaceCount)}`;
+          return `${spaces}- ${key}: ${getValue(node.value, increasedDepth)}`;
         case 'unchanged':
-          return `${spaces}  ${key}: ${getValue(node.value, deepSpaceCount)}`;
+          return `${spaces}  ${key}: ${getValue(node.value, increasedDepth)}`;
         default:
           throw new Error(`Formatter 'stylish' don't support this '${status}' node status`);
       }
     });
-
-    return [
-      '{',
-      ...lines,
-      `${currentSpaces}}`,
-    ].join('\n');
+    return getOutputTree(lines, getCurrentSpaces(depth));
   };
-
   return iter(data, 0);
 };
 
