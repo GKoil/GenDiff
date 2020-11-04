9f868889fb75c39715df45cfb4c2815ffed3616b
Кирилл <samsuu@ya.ru>
2020-11-04 18:40:21 +0700

    Update temp formatter stylish | fix output

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -11,20 +11,18 @@ const getSpaces = (depth) => {
 };
 
 const getValue = (value, depth) => {
-  const [currentSpaces, spaces] = getSpaces(depth);
-  console.log(value);
-  if (_.isString(value)) {
-    return value;
+  if (_.isObject(value)) {
+    const [currentSpaces, spaces] = getSpaces(depth);
+    const lines = Object
+      .entries(value)
+      .map(([key, objectValue]) => `${spaces}  ${key}: ${objectValue}`);
+    return [
+      '{',
+      ...lines,
+      `${currentSpaces}}`,
+    ].join('\n');
   }
-
-  const lines = Object
-    .entries(value)
-    .map(([key, objectValue]) => `${spaces}  ${key}: ${objectValue}`);
-  return [
-    '{',
-    ...lines,
-    `${currentSpaces}}`,
-  ].join('\n');
+  return value.toString();
 };
 
 const stylish = (data) => {
@@ -34,15 +32,15 @@ const stylish = (data) => {
       const { key, status } = node;
       switch (status) {
         case 'updated':
-          return `${spaces}- ${key}: ${getValue(node.oldValue, depth)}\n${spaces}+ ${key}: ${getValue(node.newValue, depth)}`;
+          return `${spaces}- ${key}: ${getValue(node.oldValue, deepSpaceCount)}\n${spaces}+ ${key}: ${getValue(node.newValue, deepSpaceCount)}`;
         case 'nested':
           return `${spaces}  ${key}: ${iter(node.children, deepSpaceCount)}`;
         case 'added':
-          return `${spaces}+ ${key}: ${getValue(node.value, depth)}`;
+          return `${spaces}+ ${key}: ${getValue(node.value, deepSpaceCount)}`;
         case 'deleted':
-          return `${spaces}- ${key}: ${getValue(node.value, depth)}`;
+          return `${spaces}- ${key}: ${getValue(node.value, deepSpaceCount)}`;
         case 'unchanged':
-          return `${spaces}  ${key}: ${getValue(node.value, depth)}`;
+          return `${spaces}  ${key}: ${getValue(node.value, deepSpaceCount)}`;
         default:
           throw new Error(`Formatter 'stylish' don't support this '${status}' node status`);
       }
