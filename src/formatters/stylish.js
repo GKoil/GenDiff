e48d7b4481217aaa5067a0b03363901882bbca59
Кирилл <samsuu@ya.ru>
2020-11-04 18:15:24 +0700

    Temp formatter stylish

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,45 +1,51 @@
 import _ from 'lodash';
 
-const getSign = {
-  added: '+',
-  deleted: '-',
-  unchanged: ' ',
+const spaceCount = 2;
+
+const getSpaces = (depth) => {
+  const replacer = ' ';
+  const deepSpaceCount = spaceCount + depth;
+  const spaces = replacer.repeat(deepSpaceCount);
+  const currentSpaces = replacer.repeat(depth);
+  return [currentSpaces, spaces, deepSpaceCount];
 };
 
-const stylish = (data, spaceCount = 2) => {
+const getValue = (value, depth) => {
+  const [currentSpaces, spaces] = getSpaces(depth);
+  console.log(value);
+  if (_.isString(value)) {
+    return value;
+  }
+
+  const lines = Object
+    .entries(value)
+    .map(([key, objectValue]) => `${spaces}  ${key}: ${objectValue}`);
+  return [
+    '{',
+    ...lines,
+    `${currentSpaces}}`,
+  ].join('\n');
+};
+
+const stylish = (data) => {
   const iter = (tree, depth) => {
-    const replacer = ' ';
-    const deepSpaceCount = spaceCount + depth;
-    const spaces = replacer.repeat(deepSpaceCount);
-    const currentSpaces = replacer.repeat(depth);
-
-    if (!_.isObject(tree)) {
-      return tree.toString();
-    }
-
-    if (!_.isArray(tree)) {
-      const lines = Object
-        .entries(tree)
-        .map(([key, value]) => `${spaces}  ${key}: ${value}`);
-      return [
-        '{',
-        ...lines,
-        `${currentSpaces}}`,
-      ].join('\n');
-    }
+    const [currentSpaces, spaces, deepSpaceCount] = getSpaces(depth);
     const lines = tree.map((node) => {
       const { key, status } = node;
-      if (status === 'updated') {
-        const oldValue = iter(node.oldValue, deepSpaceCount);
-        const newValue = iter(node.newValue, deepSpaceCount);
-        return `${spaces}- ${key}: ${oldValue}\n${spaces}+ ${key}: ${newValue}`;
+      switch (status) {
+        case 'updated':
+          return `${spaces}- ${key}: ${getValue(node.oldValue, depth)}\n${spaces}+ ${key}: ${getValue(node.newValue, depth)}`;
+        case 'nested':
+          return `${spaces}  ${key}: ${iter(node.children, deepSpaceCount)}`;
+        case 'added':
+          return `${spaces}+ ${key}: ${getValue(node.value, depth)}`;
+        case 'deleted':
+          return `${spaces}- ${key}: ${getValue(node.value, depth)}`;
+        case 'unchanged':
+          return `${spaces}  ${key}: ${getValue(node.value, depth)}`;
+        default:
+          throw new Error(`Formatter 'stylish' don't support this '${status}' node status`);
       }
-      if (status === 'nested') {
-        const children = iter(node.children, deepSpaceCount);
-        return `${spaces}  ${key}: ${children}`;
-      }
-      const value = iter(node.value);
-      return `${spaces}${getSign[status]} ${key}: ${value}`;
     });
 
     return [
